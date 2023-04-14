390b5a076a9ffb38c8a3236653d9710eb3a60928
Pedro Gabriel Schuenck bittencourt <105459717+PedroSchuenck@users.noreply.github.com>
2023-04-14 00:16:59 -0300

    Delete STRING_JS directory

--- STRING_JS/negritoExer/vogaisNegrito.js
+++
@@ -1,8 +0,0 @@
-let btexecuta = document.getElementById("executa");
-let escreve = document.getElementById("resultado");
-let fraseInput = document.getElementById("frase");
-btexecuta.addEventListener('click' ,function transforma(){
-    let frase = fraseInput.value;
-    let fraseComVogaisEmNegrito = frase.replace(/[aeiou]/gi, (vogal) => `<b>${vogal}</b>`);
-    escreve.innerHTML = `Frase com vogais em negrito: ${fraseComVogaisEmNegrito}`;
-});
