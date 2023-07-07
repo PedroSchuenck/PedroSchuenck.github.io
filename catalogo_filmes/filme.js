a4f712d155a86dc6e88af52be2f39b6005f9f2cd
Pedro Gabriel Schuenck bittencourt <105459717+PedroSchuenck@users.noreply.github.com>
2023-07-07 16:30:53 -0300

    Update filme.js

--- catalogo_filmes/filme.js
+++ catalogo_filmes/filme.js
@@ -138,15 +138,10 @@ https.onreadystatechange = function() {
             for (let i = 0; i < fullStars; i++) {
                     stars += "&#9733;"; // Código HTML para uma estrela cheia
                 }
-            
-            if (halfStar) {
-                stars += "&#9733;&#189;"; // Código HTML para uma estrela cortada ao meio
-                } else {
                 for (let i = fullStars; i < 5; i++) {
                     stars += "&#9734;"; // Código HTML para uma estrela vazia
                 }
-                }
-            
+                }  
                 return stars;
             }
         });
