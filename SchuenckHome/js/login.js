58e4a55d25ea5fbbaa0c510d586ba0cabd0c0fd5
Pedro Gabriel Schuenck bittencourt <105459717+PedroSchuenck@users.noreply.github.com>
2025-05-04 21:08:50 -0300

    A volta do projeto

--- SchuenckHome/js/login.js
+++ SchuenckHome/js/login.js
@@ -1,20 +1,46 @@
-
 let button_enviar_form = document.getElementById("submit-form");
 let form = document.getElementById("form");
-let usarname_form = document.getElementById("username").value;
-let password_form = document.getElementById("password").value;
 let button_reset = document.getElementById("reset-form");
-button_enviar_form.addEventListener('click', (e) => {
+let modal_login = document.getElementById("modal-login");
+let fechar_modal = document.querySelector(".bi-x-lg");
+let texto_modal_login = document.getElementById("text-modal-login");
 
-    console.log()
-    if (usarname_form == "Pedro" || usarname_form == "Marcia" || usarname_form == "Marcio" || usarname_form == "João" || usarname_form == "Beatriz") {
-        if(password_form == "root"){
-            alert("Usuário Correto");
-        }
-        form.action = "./SchuenckHome/index.html";
-    } else {
-        alert("Usuário ou senha, errado.");
-        e.preventDefault();
+
+button_enviar_form.addEventListener('click', (e) => {
+    let usarname_form = document.getElementById("username").value.toLowerCase();
+    let password_form = document.getElementById("password").value;
+    modalLogin(usarname_form,password_form);
+})
+
+fechar_modal.addEventListener('click', ()=>{
+    modal_login.style.opacity = "0";
+    
+})
+ 
+//função modal login
+function modalLogin(usuario, senha){
+    if(usuario !== "root" && senha !== "1234"){
+        console.log("entrou")
+        texto_modal_login.innerHTML = "Usuário e Senha, Incorretos!";
+        texto_modal_login.style.color = "red";
+        modal_login.style.opacity = "1";
+
+    }
+    else if(usuario !== "root"){
+        texto_modal_login.innerHTML = "Seu Usuário está incorreto!";
+        texto_modal_login.style.color = "red";
+        modal_login.style.opacity = "1";
+
+    }
+    else if(senha != "1234"){
+        texto_modal_login.innerHTML = "Sua senha esta incorreta!";
+        texto_modal_login.style.color = "red";
+        modal_login.style.opacity = "1";
+    }
+    else{
+        console.log("aqui")
+        texto_modal_login.innerHTML = "Login efetuado com sucesso";
+        texto_modal_login.style.color = "green";
+        modal_login.style.opacity = "1";
     }
 }
-)
